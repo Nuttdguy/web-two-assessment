d740f28441fd79da7577620543898be23a9d4e3c
Phouthalang Pygnasak <phou@Phouthalangs-MacBook-Pro.local>
2017-09-13 15:17:22 -0700

    create and format user object from .csv

--- js-assessment.js
+++ js-assessment.js
@@ -5,84 +5,107 @@ fetch("MOCK_DATA.csv").then(function (response) {
     }).then(function (responseText) {
 
         // console.log(response); // log the response, check the content
-        formatCsv(responseText); // function that will format result
+        formatCsvFrom(responseText); // function that will format result
 
     }).catch(function(error) {
         console.log(error)
 });
 
-function formatCsv(responseText) {
+//===|| 001
+function formatCsvFrom(responseText) {
 
-    const userArr = getFormattedResponse(responseText, "\n");
-    const userKeys = getKeyValues(userArr, ",");
+    const userArr = constructArrayFrom(responseText, "\n");
+    const userKeys = getKeyValuesFrom(userArr, ",");
 
     let userObj = [] // initialize array to hold user objects
     for (let idx = 1; idx < userArr.length; idx++) {
 
         let currentUserRow = userArr[idx].split(",");
-        userObj.push(createUserObj(currentUserRow, userKeys)); // for each key, call the method and pass in the current key
+        userObj.push(createUserObjectsFrom(currentUserRow, userKeys)); // for each key, call the method and pass in the current key
     }
 
-    // create another function to format data
-    // console.log(userObj.length);
-    formatUserObj(userObj);
-    // console.log(userObj);
-
+    formatUserObjectFrom(userObj);
 }
 
-function getFormattedResponse(responseText, deliminator) {
+//===|| 002
+function constructArrayFrom(responseText, deliminator) {
     return responseText.split(deliminator);
 }
 
-function getKeyValues(userArr, deliminator) {
+//===|| 003
+function getKeyValuesFrom(userArr, deliminator) {
     return userArr[0].split(deliminator);
 }
 
-function createUserObj(currentUserRow, userKeys) {
+//===|| 004
+function createUserObjectsFrom(currentUserRow, userKeys) {
     let userObj = { }
 
     for (let col = 0; col < currentUserRow.length; col++ ) {
         userObj[userKeys[col]] = currentUserRow[col];
     }
-
     return userObj;
 }
 
-function formatUserObj(userObj) {
+//===|| 005
+function formatUserObjectFrom(userObj) {
     let userFormattedArr = [];
 
-    // input == string >> output to correct format || 1001 objects in an array
-    for (let idx = 0; idx < userObj.length; idx++) {
+    let userObjWithAddedKeys = addKeysTo(userObj); // add additional meta data "keys" to object
 
-        let row = userObj[idx]; // Stores current row into a variable
-        let tempObj = {}; // Create an empty object to store key:value pairs
-
-        for (let description in row) {
-            switch (description) {
-                case 'age':
-                    tempObj[description] = Number.parseInt(row[description]);
-                    break;
-                case 'date':
-                    tempObj[description] = formatDate(row[description]);
-                    break;
-                case 'id':
-                    tempObj[description] = Number.parseInt(row[description]);
-                    break;
-                case 'rating':
-                    tempObj[description] = Number.parseFloat(row[description]);
-                    break;
-                default:
-                    tempObj[description] = row[description];
-                    break;
-            }
-        }
-        userFormattedArr.push(tempObj);
+    for (let idx = 0; idx < userObjWithAddedKeys.length - 1; idx++) {
+        let userRow = userObjWithAddedKeys[idx]; // Stores current row into a variable
+        userFormattedArr.push(assignValuesTo(userRow));
     }
     console.log(userFormattedArr);
     return userFormattedArr;
 }
 
-function formatDate(date) {
+//===|| 006
+function addKeysTo(userObj) {
+
+    for (let row = 0; row < userObj.length -1; row++) {
+        // access object > object index > add key > then value
+        userObj[row]['fullName'] = '';
+        userObj[row]['rating2'] = 0;
+    }
+    return userObj;
+}
+
+//===|| 007
+function assignValuesTo(userRow) {
+    let tempObj = { } // Create an empty object to store key:value pairs
+
+    for (let colDescription in userRow) {
+        switch (colDescription) {
+            case 'age':
+                tempObj[colDescription] = Number.parseInt(userRow[colDescription]);
+                break;
+            case 'date':
+                tempObj[colDescription] = formatDateTo(userRow[colDescription]);
+                break;
+            case 'id':
+                tempObj[colDescription] = Number.parseInt(userRow[colDescription]);
+                break;
+            case 'rating':
+                tempObj[colDescription] = Number.parseFloat(userRow[colDescription]);
+                break;
+            case 'rating2':
+                tempObj[colDescription] = Number.parseFloat(userRow['rating']).toFixed(2).toString();
+                break;
+            case 'fullName':
+                tempObj[colDescription] = userRow['prefix'] + ' ' + userRow['first_name'] + ' ' + userRow['last_name'];
+                break;
+            default:
+                tempObj[colDescription] = userRow[colDescription];
+                break;
+        }
+    }
+    return tempObj;
+}
+
+//===|| 008
+function formatDateTo(date) {
     let newDate = Number.parseInt(date); // format into number
     return new Date(newDate);
 }
